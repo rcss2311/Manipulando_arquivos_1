ed96a789c47642ea622ea046d69373ac2a90c009
Ricardo Souza <rcss2311@gmail.com>
2020-11-03 09:51:38 -0300

    Segunda forma de resolução

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,59 +6,66 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Products;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
-		String [] produtos = new String[] {"TV LED,1290.99,1","Video Game Chair,350.50,3","Iphone X,900.00,2","Samsung Galaxy 9,850.00,2"};
-		String caminho = "c://WS-Eclipse//Itens_Vendidos.csv";
-		boolean success = new File("c://WS-Eclipse" + "\\out").mkdir();
-		String caminho2 = "c://WS-Eclipse//out//summary.csv";
-		
-		
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter(caminho))) {
-			for(String p: produtos) {
-				bw.write(p);
-				bw.newLine();
-			}
-			
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		} 
-		
-		try(BufferedReader br = new BufferedReader(new FileReader(caminho))){
-			String lista = br.readLine();
-			String[] vect;
-			
-			
-			try(BufferedWriter bw = new BufferedWriter(new FileWriter(caminho2))){
-				
-				while(lista != null) {
-					vect = lista.split(",");
-					String word1 = vect[0];
-					String word2 = vect[1];
-					String word3 = vect[2];
-				
-					double total = Double.parseDouble(word2)* Double.parseDouble(word3);
-				
-					bw.write(word1);
-					bw.write(", ");
-					bw.write(String.valueOf(total));
-					bw.newLine();
-				
-					lista = br.readLine();
-				}
-				
-			}
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-	}
+	public static void main(String[] args) throws ParseException {
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<Products> list = new ArrayList<>();
+
+		System.out.println("Enter file path: ");
+		String sourceFileStr = sc.nextLine();
+
+		File sourceFile = new File(sourceFileStr);
+		String sourceFolderStr = sourceFile.getParent();
+		
+		boolean success = new File(sourceFolderStr + "\\out").mkdir();
+		
+		String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+
+			String itemCsv = br.readLine();
+			while (itemCsv != null) {
+
+				String[] fields = itemCsv.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+
+				list.add(new Products(name, price, quantity));
+
+				itemCsv = br.readLine();
+			}
+
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+				for (Products item : list) {
+					bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+					bw.newLine();
+				}
+
+				System.out.println(targetFileStr + " CREATED!");
+				
+			} catch (IOException e) {
+				System.out.println("Error writing file: " + e.getMessage());
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+
+		sc.close();
+	}
 }
+
